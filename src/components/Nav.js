b32a4172336dc8e66a9220589eb6a30bc07daa7f
zenlink <zheng_liu@hotmail.com>
2021-11-06 22:43:40 -0400

    1.03

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,21 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from "../AppContext";
+import {FaGithub} from "react-icons/fa";
+import { mergeClasses } from '@material-ui/styles';
 
 
 
 const Nav = () => {
     const [state, setState] = useContext(AppContext);
    
+    const handleGithubClick = () => {
+        window.location.href = "https://github.com/zenlink/Expenses-Manager"
+    }
     return (
         <div className="navHead">
-            <div className="title" onClick={() => { setState(s => { return { ...s, currentComponentIndex: 0, activeButton: 0 } }) }}>{state.navArr[0]}</div>
+
+            <div className="title" onClick={() => { setState(s => { return { ...s, currentComponentIndex: 0, activeButton: 0 } }) }}>{state.navArr[0]}
+            <span className ="githubicon"><FaGithub onClick ={handleGithubClick}/></span></div>
             <div className="navLinks">
                 {state.navArr.map((x, index) => {
                     return (
